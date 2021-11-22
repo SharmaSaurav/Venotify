001ead354a4818daf2eaf5a9d59872a3988e9a79
DhruvKauts2 <kautsdhruv15@gmail.com>
2021-11-22 17:10:13 +0300

    atlas mongo linked

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')  // Because we need express
 const app = express()   //Instantiate express and call the object "app"
+const mongoose = require('mongoose')    //Because we need mongoose for the database
+
+const DB = "mongodb+srv://venotify:venotify123@cluster0.bxgwe.mongodb.net/venotifyDB?retryWrites=true&w=majority"   //Database link + authentication
+
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('connection successful');
+  }).catch((err)=>console.log('Connection Failed'));
 
 app.listen(5000,() => {
     console.log('server is running on port 5000');
