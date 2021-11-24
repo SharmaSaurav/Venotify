b3e9d9a2310817825311de5065ae20cf15e2d4c8
Saniya Singla <78790981+SaniyaSingla@users.noreply.github.com>
2021-11-24 11:38:13 +0530

    Update clubs.dart

--- clubs.dart
+++ clubs.dart
@@ -3,7 +3,7 @@ import 'package:venotify/src/login_signup/signup.dart';
 
 List<MultiSelectDialogItem<int>> clubs = [];
 
-var clubsinlist = {
+final clubsinlist = {
   1: 'Art and Photography Club (APC)',
   2: 'Music Club',
   3: 'Dramatics Club',
@@ -16,19 +16,20 @@ var clubsinlist = {
   10: 'Hindi Editorial Board',
   11: 'Entrepreneurship and Incubment Cell (EIC)',
   12: 'Student Counselling Cell (SCC)',
-  13: 'Aerospace Technical Society (ATC)',
-  14: 'American Society of Civil Engineers (ASCE)',
-  15: 'Indian Institute of Metals (IIM)',
-  16: 'Astronomy and Space Physics Society (ASPS)',
-  17: 'Institute of Electronics and Electrical Engineers (IEEE)',
-  18: 'Society of Automotive Engineers (SAE)',
-  19: 'Institution of Electronics and Telecommunication Engineers (IETE)',
-  20: 'Society Of Manufacturing Engineers (SME)',
-  21: 'Solar Energy Society of India (SESI)',
-  22: 'Robotics Society',
-  23: 'American Society of Mechanical Engineers (ASME)',
-  24: 'NSS',
-  25: 'NCC',
+  13: 'ACM CSS',
+  14: 'Aerospace Technical Society (ATC)',
+  15: 'American Society of Civil Engineers (ASCE)',
+  16: 'Indian Institute of Metals (IIM)',
+  17: 'Astronomy and Space Physics Society (ASPS)',
+  18: 'Institute of Electronics and Electrical Engineers (IEEE)',
+  19: 'Society of Automotive Engineers (SAE)',
+  20: 'Institution of Electronics and Telecommunication Engineers (IETE)',
+  21: 'Society Of Manufacturing Engineers (SME)',
+  22: 'Solar Energy Society of India (SESI)',
+  23: 'Robotics Society',
+  24: 'American Society of Mechanical Engineers (ASME)',
+  25: 'NSS',
+  26: 'NCC',
 };
 
 void populateclubs() {
