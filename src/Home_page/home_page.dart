ea048fd603f59bd5c7cb214ae11ac847ca17c1f2
Saniya Singla <78790981+SaniyaSingla@users.noreply.github.com>
2021-11-27 19:19:19 +0530

    Update home_page.dart

--- src/Home_page/home_page.dart
+++ src/Home_page/home_page.dart
@@ -1,128 +1,74 @@
+//import 'dart:html';
+
 import 'package:flutter/material.dart';
-import 'dart:async';
-import '../resources/repo.dart';
-import '../models/item_model.dart';
-import 'package:intl/intl.dart';
-import '../resources/repo.dart';
-import 'dart:developer';
+import 'new_announcement.dart';
+import 'home_screen.dart';
+import 'profile_page.dart';
 
-Repository rep = Repository();
-AnnouncementModel? announcement;
-
-class HomePage extends StatefulWidget {
+class homePage extends StatefulWidget {
   createState() {
-    return HomePageState();
+    return homepageState();
   }
 }
 
-class HomePageState extends State<HomePage> {
-  // final formKey = GlobalKey<FormState>();
 
-  String date = '';
+class homepageState extends State<homePage> {
 
-  Widget build(context) {
-    return SingleChildScrollView(
-        child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-          Container(
-            // margin: EdgeInsets.only(top: 200.0, left: 20, right: 20),
-            child: Column(
-              children: [
-                Container(
-                  height: 280,
-                  width: 300,
-                  margin: EdgeInsets.only(left: 50, right: 50, top: 30),
-                  color: Colors.white,
-                  child: Column(
-                    children: [
-                      Image(
-                        image: NetworkImage(
-                            'https://yt3.ggpht.com/ytc/AKedOLT3xopdGe6plCeD_YoWlrMTmPoQaJU1lKDVPLV9=s900-c-k-c0x00ffffff-no-rj'),
-                        height: 240,
-                        width: 300,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: Text(
-                          'ACM CSS',
-                          style: TextStyle(
-                            fontSize: MediaQuery.of(context).size.height / 50,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.indigo[600],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                // Padding(padding: EdgeInsets.all(50.0)),
-                Container(
-                  child: _buildLogo(),
-                )
-              ],
-            ), //column
-          ), // Form
-        ]));
-    //container
+  int _selectedIndex = 0;
+
+  // List<Widget> _widgetOptions = <Widget>[
+  // HomePage(),
+  // AnnouncementPage(),
+  // profilePage(),
+  // ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      if(index == 1){
+        Navigator.push(
+          context, MaterialPageRoute(builder: (context) => AnnouncementPage()));
+      }
+      else if(index == 2){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => profilePage()));
+      }
+      else {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+      }
+    });
   }
 
-  Widget _buildLogo() {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Container(
-            padding: const EdgeInsets.only(top: 20, right: 150),
-            child: (Text(
-              'Your Announcements',
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height / 40,
-                fontWeight: FontWeight.bold,
-                color: Colors.indigo[600],
-              ),
-            )
-                // have to add a date widget here in row
-                )),
-        // Container(),
-        announcements(),
-        announcements(),
-        announcements(),
-        // just for testing the api data
-        Container(
-            margin: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-            child: ElevatedButton(
-              onPressed: () => showConsoleUsingPrint(),
-              child: Text(' Print Console Message using Print '),
-            ))
-      ],
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple.shade100,
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            (BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'home',
+              //activeIcon: HomePage(),
+            )),
+            (BottomNavigationBarItem(
+              icon: Icon(Icons.add_box),
+              label: 'notif',
+              //activeIcon: AnnouncementPage(),
+            )),
+            (BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'you',
+            )),
+          ],
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.purple.shade900,
+          unselectedItemColor: Colors.grey.shade300,
+          iconSize: 24.0,
+          elevation: 8.0,
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+        body: HomePage(),
+      ),
     );
   }
 }
-
-Widget announcements() {
-  return Container(
-    margin: EdgeInsets.only(top: 25.0, left: 20, right: 150),
-    padding: const EdgeInsets.all(20),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-      color: Colors.white,
-    ),
-    // child: (Text('pic_of_platform  title   date')),
-    child: Row(children: [
-      Icon(Icons.alarm),
-      Text('      Ideathon      28-11-21'),
-    ]),
-  );
-}
-
-Future<void> data() async {
-  announcement = await rep.fetchAnnouncments();
-  print(announcement!.Organiser);
-}
-
-void showConsoleUsingPrint() {
-  data(); // should print out itemModel
-}
-
-
-// saniya's code
